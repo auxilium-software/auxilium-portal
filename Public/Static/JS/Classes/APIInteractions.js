9f6cac8f25467e4770433fbc2498932dc76723c2
Cerys <cerysamberlewis@gmail.com>
2025-09-20 16:29:16 +0100

    can now load in all cases yay

--- Public/Static/JS/Classes/APIInteractions.js
+++ Public/Static/JS/Classes/APIInteractions.js
@@ -7,15 +7,18 @@ class APIInteractions
             "credentials": 'include',
         };
 
-        if (method === 'POST' || method === 'PATCH') {
-            headers["Content-Type"] = "application/json"; // Ensure JSON format
+        if (method === 'POST' || method === 'PATCH')
+        {
+            headers["Content-Type"] = "application/json";
         }
 
         return headers;
     }
 
-    async #apiRequest(method, target, useAuth, payload = null) {
-        try {
+    async #apiRequest(method, target, useAuth, payload = null)
+    {
+        try
+        {
             const options = {
                 method: method,
                 headers: this.#getHeaders(method, useAuth),
@@ -25,35 +28,39 @@ class APIInteractions
 
             const response = await fetch(`http://localhost:1983${target}`, options);
 
-            // Check if the response is ok
-            if (!response.ok) {
+            if (!response.ok)
+            {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
 
             return [response.status, await response.json()];
-        }
-        catch (error)
+        } catch (error)
         {
             return [null, error.message];
         }
     }
 
-    async API_PATCH(target, payload, useAuth = true) {
+    async API_PATCH(target, payload, useAuth = true)
+    {
         return await this.#apiRequest('PATCH', target, useAuth, payload);
     }
 
-    async API_POST(target, payload, useAuth = true) {
+    async API_POST(target, payload, useAuth = true)
+    {
         return await this.#apiRequest('POST', target, useAuth, payload);
     }
 
-    async API_GET(target, useAuth = true) {
+    async API_GET(target, useAuth = true)
+    {
         return await this.#apiRequest('GET', target, useAuth);
     }
 
-    async API_DELETE(target, useAuth = true) {
+    async API_DELETE(target, useAuth = true)
+    {
         return await this.#apiRequest('DELETE', target, useAuth);
     }
-    async API_FILE_UPLOAD(target, file, useAuth=true)
+
+    async API_FILE_UPLOAD(target, file, useAuth = true)
     {
         const formData = new FormData();
         formData.append('file', file);
