6e13c23eb5c5eb56d138338bc383607545d05f26
Cerys <cerysamberlewis@gmail.com>
2025-10-22 15:58:35 +0100

    now displays your cases on the dashboard

--- Public/Static/JS/APIControllers/CaseController.js
+++ Public/Static/JS/APIControllers/CaseController.js
@@ -36,6 +36,73 @@ class CaseController {
             }
         }
 
+        return allCases;
+    }
+
+    async GetAllMyCases_DontDoThisLol()
+    {
+        let allCases = [];
+
+        let pageNumber = 1;
+        const perPage = 100;
+
+        while (true)
+        {
+            const [statusCode, payload] = await this.#APIInstance.API_GET(
+                `/api/v3/cases/mine?page=${pageNumber}&per_page=${perPage}`,
+                true,
+            );
+            if(statusCode === 200)
+            {
+                for (const temp of payload['data'])
+                {
+                    allCases.push(temp);
+                }
+                pageNumber ++;
+                if(payload['has_more'] !== true)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return allCases;
+    }
+
+    async GetAllAssignedCases_DontDoThisLol()
+    {
+        let allCases = [];
+
+        let pageNumber = 1;
+        const perPage = 100;
+
+        while (true)
+        {
+            const [statusCode, payload] = await this.#APIInstance.API_GET(
+                `/api/v3/cases/assigned?page=${pageNumber}&per_page=${perPage}`,
+                true,
+            );
+            if(statusCode === 200)
+            {
+                for (const temp of payload['data'])
+                {
+                    allCases.push(temp);
+                }
+                pageNumber ++;
+                if(payload['has_more'] !== true)
+                {
+                    break;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
 
         return allCases;
     }
--- Public/dashboard.php
+++ Public/dashboard.php
@@ -2,6 +2,7 @@
 
 use Auxilium\Enumerators\CookieKey;
 use Auxilium\Exceptions\DatabaseConnectionException;
+use Auxilium\ServiceInteractions\APIInteractions;
 use Auxilium\SessionHandling\CookieHandling;
 use Auxilium\TwigHandling\PageBuilder;
 use Auxilium\Utilities\SecurityUtilities;
@@ -13,8 +14,17 @@ try
 {
     SecurityUtilities::RequireLogin();
 
+    $cases = APIInteractions::Get(
+        endpoint: '/cases/mine',
+    )->Payload;
+    $messages = APIInteractions::Get(
+        endpoint: '/messages/',
+    )->Payload;
+
     PageBuilder::AutoRender(variables: [
         "is_admin" => SecurityUtilities::IsAdmin(),
+        "MyCases" => $cases,
+        "MyMessages" => $messages,
     ]
     );
 }
