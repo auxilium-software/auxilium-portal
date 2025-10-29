6d82bb304c92e09b970101bc136ce1e3039c14b8
Cerys <cerysamberlewis@gmail.com>
2025-10-29 12:29:43 +0000

    Update case-overview.php

--- RoutedPages/case-overview.php
+++ RoutedPages/case-overview.php
@@ -17,23 +17,6 @@ try
         endpoint: '/cases/' . URIParsingUtilities::GetUUIDFromURI(index: 0),
     )->Payload;
 
-    foreach($caseData['clients'] as &$clientID)
-    {
-        $workerDetails = APIInteractions::Get(
-            endpoint: '/users/' . $clientID,
-        );
-        $clientID = $workerDetails->Payload;
-    }
-    unset($clientID);
-    foreach($caseData['workers'] as &$workerID)
-    {
-        $workerDetails = APIInteractions::Get(
-            endpoint: '/users/' . $workerID,
-        );
-        $workerID = $workerDetails->Payload;
-    }
-    unset($workerID);
-
     PageBuilder::Render(
         template: '/VirtualPages/CaseOverviewPage.html.twig',
         variables: [
