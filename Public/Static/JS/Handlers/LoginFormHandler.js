62d55a440e6a25dbbb7ce72c9badbd32f222b390
Cerys <cerysamberlewis@gmail.com>
2025-11-15 12:14:21 +0000

    Update LoginFormHandler.js

--- Public/Static/JS/Handlers/LoginFormHandler.js
+++ Public/Static/JS/Handlers/LoginFormHandler.js
@@ -42,8 +42,8 @@ class LoginFormHandler {
 
             if(response !== false)
             {
-                CookieUtilities.setCookie('access_token', response['access_token'], 0.0208333);
-                CookieUtilities.setCookie('refresh_token', response['refresh_token'], 7);
+                CookieUtilities.setCookie('access_token', response['access_token'], 30);
+                CookieUtilities.setCookie('refresh_token', response['refresh_token'], 60 * 24 * 7);
                 new ToastNotification(await Localisation.translate('Login successful! Redirecting...'), 'check-circle', 'success');
 
                 setTimeout(() => {
