7186c0d96f644a17abb2698a90677b9560904dd5
Cerys <cerysamberlewis@gmail.com>
2026-03-16 14:42:49 +0000

    php stuff

---
+++ Public/admin-console.php
@@ -0,0 +1,23 @@
+<?php
+
+
+use Auxilium\ServiceInteractions\APIInteractions;
+use Auxilium\TwigHandling\PageBuilder;
+use Auxilium\Utilities\AdminConsoleUtilities;
+use Auxilium\Utilities\ConfigurationUtilities;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+try
+{
+    PageBuilder::AutoRender(
+        variables: [
+            ...AdminConsoleUtilities::GrabVariablesToPassIntoTwig(),
+            "QuickActions" => ConfigurationUtilities::GetAdminConsoleQuickActions()
+        ]
+    );
+}
+catch(Exception $e)
+{
+    PageBuilder::RenderInternalSystemError($e);
+}
---
+++ Public/admin-console/object-management/user-management/all-users.php
@@ -0,0 +1,20 @@
+<?php
+
+
+use Auxilium\ServiceInteractions\APIInteractions;
+use Auxilium\TwigHandling\PageBuilder;
+use Auxilium\Utilities\AdminConsoleUtilities;
+use Auxilium\Utilities\ConfigurationUtilities;
+
+require_once __DIR__ . '/../../../../vendor/autoload.php';
+
+try
+{
+    PageBuilder::AutoRender(
+        variables: AdminConsoleUtilities::GrabVariablesToPassIntoTwig()
+    );
+}
+catch(Exception $e)
+{
+    PageBuilder::RenderInternalSystemError($e);
+}
