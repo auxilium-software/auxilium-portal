2132a050191b1dbba6eb8fa71da67b8fa7f9df06
Cerys <cerysamberlewis@gmail.com>
2026-04-15 03:02:17 +0100

    Update LoginFormHandler.js

--- Public/Static/JS/Handlers/LoginFormHandler.js
+++ Public/Static/JS/Handlers/LoginFormHandler.js
@@ -96,17 +96,17 @@ class LoginFormHandler
                 recaptchaToken
             );
 
+            if (response.mustChangePassword && response.mustChangePassword === true)
+            {
+                this.passwordChangeToken = response.passwordChangeToken;
+                this.showPasswordChangeForm();
+                return;
+            }
             if (response.mfaRequired && response.mfaRequired === true)
             {
                 // MFA required - store token and show MFA form
                 this.mfaSessionToken = response.mfaSessionToken;
                 this.showMfaForm();
-                return;
-            }
-            if (response.mustChangePassword && response.mustChangePassword === true)
-            {
-                this.passwordChangeToken = response.passwordChangeToken;
-                this.showPasswordChangeForm();
                 return;
             }
 
