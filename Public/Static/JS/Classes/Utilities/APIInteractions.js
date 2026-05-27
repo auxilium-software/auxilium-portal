ce76bfef3f10dfce897abe57d54adf2dcd17ffb3
Cerys <cerysamberlewis@gmail.com>
2026-05-27 14:03:52 +0100

    Update APIInteractions.js

--- Public/Static/JS/Classes/Utilities/APIInteractions.js
+++ Public/Static/JS/Classes/Utilities/APIInteractions.js
@@ -91,10 +91,12 @@ class APIInteractions
                 );
             }
 
+            /*
             if (!response.ok)
             {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
+            */
 
             return [response.status, await response.json()];
         }
