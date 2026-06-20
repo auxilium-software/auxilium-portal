6fd7f91843f4f0fcdcd1d8b02826f06d7c042699
Cerys <cerysamberlewis@gmail.com>
2026-06-20 12:06:07 +0100

    Update WemwbsController.js

--- Public/Static/JS/APIControllers/WemwbsController.js
+++ Public/Static/JS/APIControllers/WemwbsController.js
@@ -11,31 +11,30 @@ class WemwbsController
         let allAssessments = [];
 
         let pageNumber = 1;
-        const pageSize = 1000;
+        const pageSize = 500;
 
         while (true)
         {
             const [statusCode, payload] = await this.#APIInstance.API_GET(
                 `/api/v3/wemwbs?page=${pageNumber}&pageSize=${pageSize}`
             );
-            if(statusCode === 200)
-            {
-                for (const temp of payload['assessments'])
-                {
-                    allAssessments.push(temp);
-                }
-                pageNumber ++;
-                if(payload['hasMore'] !== true)
-                {
-                    break;
-                }
-            }
-            else
+
+            if (statusCode !== 200)
             {
                 return false;
             }
-        }
 
+            for (const assessment of payload['assessments'])
+            {
+                allAssessments.push(assessment);
+            }
+
+            if (payload['hasMore'] !== true)
+            {
+                break;
+            }
+            pageNumber++;
+        }
 
         return allAssessments;
     }
