7af2da9e6bac68d4b718a951a324ad6bbc70d8ff
Cerys <cerysamberlewis@gmail.com>
2026-06-22 14:04:12 +0100

    bits and bobs

--- Public/Static/JS/APIControllers/SystemMetricController.js
+++ Public/Static/JS/APIControllers/SystemMetricController.js
@@ -16,4 +16,24 @@ class SystemMetricController {
         const [statusCode, response] = await this.#APIInstance.API_GET('/api/v3/system-metrics/lfs-size');
         return response;
     }
+    async GetApiCpuUsageMetrics()
+    {
+        const [statusCode, response] = await this.#APIInstance.API_GET('/api/v3/system-metrics/api-cpu-usage');
+        return response;
+    }
+    async GetApiRamUsageMetrics()
+    {
+        const [statusCode, response] = await this.#APIInstance.API_GET('/api/v3/system-metrics/api-ram-usage');
+        return response;
+    }
+    async GetTaskRunnerCpuUsageMetrics()
+    {
+        const [statusCode, response] = await this.#APIInstance.API_GET('/api/v3/system-metrics/task-runner-cpu-usage');
+        return response;
+    }
+    async GetTaskRunnerRamUsageMetrics()
+    {
+        const [statusCode, response] = await this.#APIInstance.API_GET('/api/v3/system-metrics/task-runner-ram-usage');
+        return response;
+    }
 }
